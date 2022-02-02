b45b5b0daebde01d99a45c922e8465ddeaf80a16
franklin <forsonfranklin@gmail.com>
2022-02-02 19:59:51 +0000

    creating the binary search tree class

---
+++ src/main/java/dataStructures/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package dataStructures.BinarySearchTree;
+
+public class BinarySearchTree {
+
+    private class Node{
+        int data;
+        Node right;
+        Node left;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+    }
+}
