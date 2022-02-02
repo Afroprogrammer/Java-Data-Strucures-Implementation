0d8c8d14e6d0a9564b6e4ccb81563341c0b9a658
franklin <forsonfranklin@gmail.com>
2022-02-02 20:09:22 +0000

    created the add to node method

--- src/main/java/dataStructures/BinarySearchTree/BinarySearchTree.java
+++ src/main/java/dataStructures/BinarySearchTree/BinarySearchTree.java
@@ -13,6 +13,26 @@ public class BinarySearchTree {
 
     }
 
+    Node root;    //a property of the binary search tree class
+
+    //add method of the binary tree
+    public void add(int data){
+        root = addToNode(root, data);     //adding using the recursion method
+    }
+
+    private Node addToNode(Node root, int data)  //a method that adds to the node
+    {
+        if(root == null){           //if the root is null create a new root and append the data
+            root = new Node(data);
+            return root;
+        }
+        if(data < root.data){
+            root.left = addToNode(root.left, data);
+        } else if(data > root.data){
+            root.right = addToNode(root.right, data);
+        }
+        return root;
+    }
 
 
     public static void main(String[] args) {
