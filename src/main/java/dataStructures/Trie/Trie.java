7300f46deb79749012e058cabdc284ae7f76284b
franklin <forsonfranklin@gmail.com>
2022-02-05 08:41:10 +0000

    implemented the insert function for a trie data structure

--- src/main/java/dataStructures/Trie/Trie.java
+++ src/main/java/dataStructures/Trie/Trie.java
@@ -1,4 +1,70 @@
 package dataStructures.Trie;
 
+import org.w3c.dom.Node;
+
+import java.util.Locale;
+
 public class Trie {
+    TrieNode root;
+
+    public Trie(){
+        root = new TrieNode(); // root is empty
+    }
+    private class TrieNode{
+        TrieNode [] children;
+        boolean isWord;
+        int capacity = 26;
+
+        public TrieNode(){
+            this.children = new TrieNode[capacity];  //capacity for storing english words
+            this.isWord = false;
+        }
+    }
+
+    public void insert (String word){
+        if(word == null || word.isBlank()|| word.isEmpty()){
+            throw  new IllegalArgumentException("invalid input ");
+        }
+        word = word.toLowerCase();
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c -'a';   //finding the index of the trie node
+            if (current.children[index] == null){
+                TrieNode node = new TrieNode();
+                current.children[index] = node;
+                current = node;
+            }else
+            {
+                current = current.children[index];
+            }
+            current.isWord = true;
+        }
+
+    }
+
+    private TrieNode getNode(String word){
+        TrieNode current = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if(current.children[index] == null){
+                return null;
+            }else{
+                current = current.children[index];
+            }
+        }
+        return current;
+    }
+
+    public static void main(String[] args) {
+        Trie newTrie = new Trie();
+        newTrie.insert("cat");
+        newTrie.insert("cab");
+        newTrie.insert("cake");
+        newTrie.insert("son");
+        newTrie.insert("son");
+        System.out.println("words added successfully");
+
+    }
 }
