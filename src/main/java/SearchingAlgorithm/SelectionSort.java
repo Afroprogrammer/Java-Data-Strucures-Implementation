72d29585e562994a2a681d1db365bb674df8c46c
franklin <forsonfranklin@gmail.com>
2022-02-06 23:54:46 +0000

    selection sort swap method implementation

--- src/main/java/SearchingAlgorithm/SelectionSort.java
+++ src/main/java/SearchingAlgorithm/SelectionSort.java
@@ -1,21 +1,43 @@
 package SearchingAlgorithm;
 
 public class SelectionSort {
+void sort(int arr[])
+{
+    int n = arr.length;
 
-    //helper function to print out an array
+    // One by one move boundary of unsorted subarray
+    for (int i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        int min_idx = i;
+        for (int j = i+1; j < n-1; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
 
-    private  static void printArray(int[] array){
-        for(int i : array){
-            System.out.print(i + " ");
-        }
+        // Swap the found minimum element with the first
+        // element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
 
+    // Prints the array
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
         System.out.println();
     }
 
-    private static  void swap( int array[], int left , int right){
-        int temp = array[left];
-        array[left] = array[right];
-        array[right] = temp;
+    // Driver code to test above
+    public static void main(String args[])
+    {
+        SelectionSort ob = new SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
     }
-
 }
